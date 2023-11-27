650952d2a2467947d20f75c3db7f1cb27298d99e
sausse93 <130512662+sausse93@users.noreply.github.com>
2023-11-27 10:56:51 +0000

    Modification de l'image de fond Home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/codefont.jpg";
+import image from "../images/fond1.jpg";
 
 const imageAltText = "Ecran montrant une partie d'un code en HTML";
 
